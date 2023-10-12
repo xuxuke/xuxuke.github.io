8644a6cb202b303f43cd1662d9d210c7350fd461
xuxuke <147708563+xuxuke@users.noreply.github.com>
2023-10-12 17:53:25 +0800

    Add files via upload

---
+++ static/js/ad.js
@@ -0,0 +1,65 @@
+function pushAd(){
+ 
+     
+
+    $('#bannerad').append(
+                '<a href="https://www.xinlangzy.com/help/" target="_blank" rel="nofollow"><img src="https://img04.sogoucdn.com/app/a/200692/1300_60_feedback_f85a877632c54053b1dd599f4f73a164.gif" style="width:100%;height:55px;border:none;" title="广告赞助"></a>'+ 
+                '<a href="http://huyazy.net/help/" target="_blank" rel="nofollow"><img src="https://img01.sogoucdn.com/app/a/200692/1250_60_feedback_089caa5d07b14ed090cb1dfa4c63e54e.gif" style="width:100%;height:55px;border:none;" title="广告赞助"></a>'+
+                '<a href="https://hongniuzy.net/help/" target="_blank" rel="nofollow"><img src="https://img04.sogoucdn.com/app/a/200692/1200_80_feedback_a87be661e3d745fcbb5f52af9af0b48a.gif" style="width:100%;height:55px;border:none;" title="广告赞助"></a>'+ 
+                '<a href="http://guangsuzy.com/help" target="_blank" rel="nofollow"><img src="https://img04.sogoucdn.com/app/a/200692/1200_60_feedback_7da19578b1534565bacd424e51459b47.gif" style="width:100%;height:55px;border:none;" title="广告赞助"></a>'+
+                '<a href="https://jyzy3.com" target="_blank" rel="nofollow"><img src="https://img01.sogoucdn.com/app/a/200692/1200_90_feedback_005f1e79e3c04324bb45b21f774ced0b.gif" style="width:100%;height:55px;border:none;" title="广告赞助"></a>'+ 
+                '<a href="https://haohuazy.com/index.php/help/index.html" target="_blank" rel="nofollow"><img src="https://img.4tuku.com/gg/haohua.gif" style="width:100%;height:120px;border:none;" title="广告赞助"></a>'+ 
+               
+             	'<a href="https://aosikazy.com/template/help/" target="_blank" rel="nofollow"><img src="https://165tchuang.com/i/2023/01/21/63cac91a34f16.gif" style="width:100%;height:55px;border:none;" title="广告赞助"></a>'+
+             	'<a href="https://naixxzy.com/help/" target="_blank" rel="nofollow"><img src="https://165tchuang.com/i/2023/04/04/642c1541243af.gif" style="width:100%;height:55px;border:none;" title="广告赞助"></a>'+
+             	'<a href="https://moduzy.cc/" target="_blank" rel="nofollow"><img src="https://165image.com:3188/dongman.gif" style="width:100%;height:55px;border:none;" title="广告赞助"></a>'+
+             	'<a href="https://moduzy.cc/" target="_blank" rel="nofollow"><img src="https://165image.com:3188/dongman.gif" style="width:100%;height:55px;border:none;" title="广告赞助"></a>'+
+             
+                '<a href="https://www.avre03.com/help/" target="_blank" rel="nofollow"><img src="https://img.4tuku.com/gg/hg.gif" style="width:100%;height:55px;border:none;" title="广告赞助"></a>'+  
+                '<a href="https://www.avre03.com/help/" target="_blank" rel="nofollow"><img src="https://img.4tuku.com/gg/hg.gif" style="width:100%;height:55px;border:none;" title="广告赞助"></a>'+  
+                '<a href="https://www.avre03.com/help/" target="_blank" rel="nofollow"><img src="https://img.4tuku.com/gg/hg.gif" style="width:100%;height:55px;border:none;" title="广告赞助"></a>'+  
+              // '<a href="https://www.taopianzy.com/home/index.html?mobb-1" target="_blank" rel="nofollow"><img src="https://img01.sogoucdn.com/app/a/200692/1300_60_feedback_91ed433b45624d86acac25fcfc55a1e8.gif" style="width:100%;height:55px;border:none;" title="广告赞助"></a>'+
+               // '<a href="https://www.taopianzy.com/home/index.html?mobb-1" target="_blank" rel="nofollow"><img src="https://img01.sogoucdn.com/app/a/200692/1300_60_feedback_91ed433b45624d86acac25fcfc55a1e8.gif" style="width:100%;height:55px;border:none;" title="广告赞助"></a>'+      
+              //  '<a href="https://vip.hai301.com/?=mobb1" target="_blank" rel="nofollow"><img src="https://img02.sogoucdn.com/app/a/200692/1300_70_feedback_ed0923765dfa45368cc29076fd416af4.gif" style="width:100%;height:55px;border:none;" title="广告赞助"></a>'+
+            
+            // 	'<a href="https://help.timizy10.cc/" target="_blank" rel="nofollow"><img src="https://img.4tuku.com/gg/tm.gif" style="width:100%;height:50px;border:none;" title="广告赞助"></a>'+
+            	'<a href="https://www.jisuzy.com" target="_blank" rel="nofollow"><img src="https://img.4tuku.com/gg/jisu.gif" style="width:100%;height:120px;border:none;" title="广告赞助"></a>'+
+            	'<a href="https://damozy2.com/" target="_blank" rel="nofollow"><img src="https://img.4tuku.com/gg/dm2.gif" style="width:100%;height:55px;border:none;" title="广告赞助"></a>'+
+            	'<a href="https://damozy2.com/" target="_blank" rel="nofollow"><img src="https://img.4tuku.com/gg/dm2.gif" style="width:100%;height:55px;border:none;" title="广告赞助"></a>'+
+            	'<a href="http://www.kuakezy.com/help/index.html" target="_blank" rel="nofollow"><img src="https://img.4tuku.com/gg/kuake.gif" style="width:100%;height:55px;border:none;" title="广告赞助"></a>'+
+                '<a href="http://www.kuakezy.com/help/index.html" target="_blank" rel="nofollow"><img src="https://img.4tuku.com/gg/kuake.gif" style="width:100%;height:55px;border:none;" title="广告赞助"></a>'+
+                '<a href="https://hongxiuzy7.com" target="_blank" rel="nofollow"><img src="https://img.4tuku.com/gg/hy.gif" style="width:100%;height:55px;border:none;" title="广告赞助"></a>'+
+            // 	'<a href="https://help.skm3u8.com/" target="_blank" rel="nofollow"><img src="https://img.4tuku.com/gg/skzy.gif" style="width:100%;height:55px;border:none;" title="广告赞助"></a>'+
+            //     '<a href="https://help.skm3u8.com/" target="_blank" rel="nofollow"><img src="https://img.4tuku.com/gg/skzy.gif" style="width:100%;height:55px;border:none;" title="广告赞助"></a>'+
+                '<a href="https://caoliuzyw.com/index.php/label/help.html" target="_blank" rel="nofollow"><img src="https://img.4tuku.com/gg/caoliu.gif" style="width:100%;height:55px;border:none;" title="广告赞助"></a>'+
+                '<a href="https://putaozyw.com/" target="_blank" rel="nofollow"><img src="https://img.4tuku.com/gg/putao.gif" style="width:100%;height:55px;border:none;" title="广告赞助"></a>'+
+                '<a href="http://kuaiyunzy.com/" target="_blank" rel="nofollow"><img src="https://img.4tuku.com/gg/kuaiyun.png" style="width:100%;height:55px;border:none;" title="广告赞助"></a>'+
+                '<a href="https://cmszyw.com/" target="_blank" rel="nofollow"><img src="https://img.cmsdh.vip/images/CMSzyw.gif" style="width:100%;height:55px;border:none;" title="广告赞助"></a>'+
+                '<a href="https://suonizy.com/" target="_blank" rel="nofollow"><img src="https://img.4tuku.com/gg/sn1.gif" style="width:100%;height:55px;border:none;" title="广告赞助"></a>'+
+                '<a href="https://suonizy.com/" target="_blank" rel="nofollow"><img src="https://img.cmsdh.vip/img/q/suonizyw2.gif" style="width:100%;height:55px;border:none;" title="广告赞助"></a>'
+                
+               
+                
+                
+    
+    );
+        
+     
+     
+     $('#wenziad').append('<a href="https://www.avre03.com/help/" target="_blank">黄瓜资源</a>'
+     );
+       $('#wenziad1').append(
+                         
+     );
+     
+      $('#downad').append(
+          '<a href="https://bjx01.com" target="_blank" rel="nofollow"><img src="https://img.4tuku.com/gg/bjx.gif" style="width:100%;height:55px;border:none;" title="广告赞助"></a>'
+          
+     );
+     
+
+}
+
+	
+     
+//.
